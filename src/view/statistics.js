fa051c5681a020bf96f9a3baf5237d5a4287f4b3
makemanback <makemanback@gmail.com>
2020-09-06 10:15:48 +0200

    добавил отображение блока статистики

---
+++ src/view/statistics.js
@@ -0,0 +1,31 @@
+import AbstractView from "./abstract.js";
+import {StatsType} from '../const.js';
+
+const createStatsItem = (type) => {
+  return type.map((statsType) => {
+    return (
+      `<div class="statistics__item statistics__item--${statsType}">
+        <canvas class="statistics__chart  statistics__chart--${statsType}" width="900"></canvas>
+      </div>`
+    )
+  }).join(``);
+};
+
+const createStatisticsTemplate = () => {
+
+  return (
+    `<section class="statistics">
+      <h2 class="visually-hidden">Trip statistics</h2>
+
+        ${createStatsItem(Object.values(StatsType))}
+
+    </section>`
+  )
+}
+
+export default class Stats extends AbstractView {
+
+  getTemplate() {
+    return createStatisticsTemplate();
+  }
+}
